45ce97dd6fe684bc6588932d5f1529a3deca7096
zkanu <zoilanurikanu@gmail.com>
2024-10-02 23:55:56 -0700

    edited inventory

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -19,12 +19,12 @@ def get_inventory():
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory" )).one()
 
-        total_potions = result.num_green_potions + result.num_blue_potions + result.num_red_potions
-        total_ml = result.num_green_ml + result.num_red_ml + result.num_blue_ml
-        total_gold = result.gold
+        # total_potions = result.num_green_potions + result.num_blue_potions + result.num_red_potions
+        # total_ml = result.num_green_ml + result.num_red_ml + result.num_blue_ml
+        # total_gold = result.gold
 
     
-    return {"number_of_potions": total_potions, "ml_in_barrels": total_ml, "gold": total_gold}
+    return {"number_of_potions": 0, "ml_in_barrels": 0, "gold": 0}
 
 # Gets called once a day
 @router.post("/plan")
